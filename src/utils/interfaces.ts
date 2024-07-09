2858dad8bc329463388ca718b23188f120585d4e
Toni David <oluwatoni.david45@gmail.com>
2024-07-09 16:32:31 -0700

    Save progress on message integration

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -1,7 +1,10 @@
 export interface User {
-  userID: string;
+  userId: string;
   username: string;
-  email: string;
+  location: string;
+  joiningDate: string;
+  itemsSold: string[]
+  itemsPurchased: string[]
   current?: boolean;
 }
 
@@ -35,4 +38,27 @@ export interface FilterOptions {
 export interface LoginCredentials {
   username:string,
   password:string
+}
+
+export interface AuthContextType {
+  user: User | null;
+  loginUser:  (username: string, password: string) => Promise<any>;
+  requestAccount: (email: string, callback: string) => Promise<void>;
+  verifyAccount: (jwt: string, username: string, password: string, location: string) => Promise<void>;
+  requestReset: (email: string, callback: string) => Promise<void>;
+  verifyReset: (jwt: string, password: string) => Promise<void>;
+  checkUserStatus: () => Promise<void>;
+}
+
+export interface SessionType {
+  user:User
+  expiration: number
+}
+
+export interface ChatType {
+  chatId: string
+  users: string[]
+  listingId: string
+  lastMessageTime: string
+  interlocutor: User
 }

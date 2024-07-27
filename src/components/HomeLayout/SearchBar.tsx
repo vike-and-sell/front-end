0a2ee3813433a33886183b6178d756abba3953b2
Brennan McMicking <brennanmcmicking@gmail.com>
2024-07-27 16:13:48 -0700

    feat: logout button

--- src/components/HomeLayout/SearchBar.tsx
+++ src/components/HomeLayout/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { Input } from "@chakra-ui/react";
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/react";
 import { FaSearch } from "react-icons/fa";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -8,30 +8,34 @@ export default function SearchBar() {
 
   const navigate = useNavigate();
   return (
-    <div className='flex items-center flex-grow gap-2 p-4'>
-      <Input
-        placeholder='Search for used...'
-        onChange={(e) => {
-          setSearchString(e.target.value);
-        }}
-        value={searchString}
-        onKeyDown={(e) => {
-          if (e.key == "Enter") {
-            navigate(`/search/${searchString}/1`);
-          }
-        }}
-      ></Input>
-      <button
-        title='Search Button'
-        className='bg-pri-blue p-3 rounded-md'
-        onClick={() => {
-          if (searchString !== "") {
-            navigate(`/search/${searchString}/1`);
-          }
-        }}
-      >
-        <FaSearch color='white' />
-      </button>
+    <div className="flex items-center flex-grow gap-2">
+      <InputGroup>
+        <Input
+          placeholder="Search for used..."
+          onChange={(e) => {
+            setSearchString(e.target.value);
+          }}
+          value={searchString}
+          onKeyDown={(e) => {
+            if (e.key == "Enter") {
+              navigate(`/search/${searchString}/1`);
+            }
+          }}
+        ></Input>
+        <InputRightElement>
+          <button
+            title="Search Button"
+            className="bg-pri-blue p-3 rounded-e-md"
+            onClick={() => {
+              if (searchString !== "") {
+                navigate(`/search/${searchString}/1`);
+              }
+            }}
+          >
+            <FaSearch color="white" />
+          </button>
+        </InputRightElement>
+      </InputGroup>
     </div>
   );
 }

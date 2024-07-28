c1e36863e9fb9feec0dac1bae9c4b91882d08cc2
Andrew Hu <andrewyuhu02@gmail.com>
2024-07-27 20:45:13 -0700

    reosolving TS errors

--- src/pages/RecomendationsPage.tsx
+++ src/pages/RecomendationsPage.tsx
@@ -1,7 +1,7 @@
 import PageHeading from "../components/PageHeading";
 import ListingsGrid from "../components/ListingsGrid";
 import { ListingCard } from "../components/ListingCard";
-import { getListingIDs } from "../utils/FakeListingsMock";
+
 import { arrayPagination } from "../utils/PaginationUtil";
 import { Listing } from "../utils/interfaces";
 import { useState, useEffect, useRef } from "react";
@@ -13,9 +13,21 @@ export default function RecomendationsPage() {
   const scrollRef = useRef<HTMLDivElement>(null);
   const { page } = useParams();
   const navigate = useNavigate();
-  const defaultListings: Listing[] = getListingIDs(); // MOCKING
+  const defaultListings: Listing[] = [
+    {
+      sellerId: "1",
+      listingId: "1",
+      title: "Bicycle for sale1",
+      price: 110.0,
+      location: "V8X",
+      status: "AVAILABLE",
+      forCharity: false,
+      listedAt: "2024-07-27T21:44:07.353627",
+      lastUpdatedAt: "2024-07-27T21:44:07.353627",
+    },
+  ]; // MOCKING
   const [currentPage, setCurrentPage] = useState(page ? +page : 1);
-  const [listings,] = useState<Listing[]>(defaultListings); // This will get replaced
+  const [listings] = useState<Listing[]>(defaultListings); // This will get replaced
   const totalPages = Math.ceil(listings.length / MAX_LISTINGS_PAGE);
 
   useEffect(() => {

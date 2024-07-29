e5f273342c6e3f0d138cf2a8143bf1701980051d
Andrew Hu <andrewyuhu02@gmail.com>
2024-07-29 11:49:25 -0700

    adds listing card scaling adjustments, fixes bug with mobile menu being overlayed weird

--- src/components/HomeLayout/Header.tsx
+++ src/components/HomeLayout/Header.tsx
@@ -12,25 +12,30 @@ export default function Header({ onClick, isActive }: HeaderProps) {
   const navigate = useNavigate();
 
   return (
-    <header className="flex gap-4 relative border-b-[0.5px] border-slate-300 bg-white lg:border-none flex-col z-30">
-      <div className=" flex items-center justify-between p-4 bg-white lg:mr-5 z-30">
+    <header className='flex gap-4 relative border-b-[0.5px] border-slate-300 bg-white lg:border-none flex-col z-30'>
+      <div className=' flex items-center justify-between p-4 bg-white lg:mr-5 z-30'>
         <img
-          className="cursor-pointer"
+          className='cursor-pointer'
           src={vikeLogo}
-          alt="Vike Logo"
+          alt='Vike Logo'
           onClick={() => {
             navigate(`/`);
           }}
         />
-        <button data-cy="ellipsis" title="ellipsis" onClick={onClick} className="lg:hidden">
-          <IoMenu size={42} color="#166aac" />
+        <button
+          data-cy='ellipsis'
+          title='ellipsis'
+          onClick={onClick}
+          className='lg:hidden'
+        >
+          <IoMenu size={42} color='#166aac' />
         </button>
       </div>
       <div
         onClick={onClick}
         className={`lg:hidden absolute left-0 top-full w-full bg-pri-blue transition-all duration-500 ${
           isActive ? "translate-y-0" : "-translate-y-full"
-        } z-10`}
+        } z-20`}
       >
         <NavigationBar></NavigationBar>
       </div>
